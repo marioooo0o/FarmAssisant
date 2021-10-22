04272fb89e6f59cb0ca4dc237bb4c3d85850a8de
Maksymilian Dendura <maksd0410@gmail.com>
2021-10-22 12:59:36 +0200

    Basic setup for front end

--- FarmAssistant/resources/views/layouts/app.blade.php
+++ FarmAssistant/resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Scripts -->
@@ -21,11 +22,11 @@
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
+            <!-- <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
-            </header>
+            </header> -->
 
             <!-- Page Content -->
             <main>
