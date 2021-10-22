4cafb555e7322b3fa7488eb6a6c4b4c0daa9c479
Maksymilian Dendura <maksd0410@gmail.com>
2021-10-22 16:06:36 +0200

    Basic layout

--- FarmAssistant/resources/views/layouts/app.blade.php
+++ FarmAssistant/resources/views/layouts/app.blade.php
@@ -11,8 +11,8 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
