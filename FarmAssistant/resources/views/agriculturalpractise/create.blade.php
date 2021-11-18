62d74d5209fcaf8ff431367a90684e46511418d3
Mariusz Dąbrowski <mariuszd49@gmail.com>
2021-11-18 16:08:30 +0100

    make basic practise validator

--- FarmAssistant/resources/views/agriculturalpractise/create.blade.php
+++ FarmAssistant/resources/views/agriculturalpractise/create.blade.php
@@ -2,24 +2,48 @@
 @section('content')
 <div class="create-container practise">
   <div class="content">
+
     <h1>Dodaj zabieg</h1>
 
-    <form action="{{ route('practise.store', [$idFarm]) }}" method="POST">
+    @if ($errors->any())
+    <div class="alert alert-danger" style="color: red">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+  @endif
+      <form action="{{ route('practise.store', [$idFarm]) }}" method="POST">
       @csrf
       <input
         type="text"
         name="practise_name"
         placeholder="Nazwa zabiegu"
+        value="{{ old('practise_name') }}"
         class="input-name"
       />
       <h2>Wybrane pola:</h2>
       <div id="fields-container">
         <select name="fields[]" class="input-field">
-          @foreach ($fields as $field)
-          <option value="{{ $field->id }}">
-            {{ $field->field_name }} {{ $field->field_area }} ha
-          </option>
-          @endforeach
+          {{-- Do zrobienia w przyszłości
+            
+            @if (old('fields')!=null)
+              @foreach (old('fields') as $fieldOld)
+                <option value="{{ $fields[$fieldOld] }}" selected>
+                  {{ $fields[$fieldOld]->field_name }} {{ $fields[$fieldOld]->field_area }} ha
+                </option>
+              @endforeach
+            @else
+            @dump('nie ma mnie')
+                @endif
+              
+            --}}
+              @foreach ($fields as $field)
+              <option value="{{ $field->id }}">
+                {{ $field->field_name }} {{ $field->field_area }} ha
+              </option>
+              @endforeach
         </select>
       </div>
       <button type="button" name="addFieldButton" id="addField">
@@ -48,7 +72,7 @@
       <button type="button" name="addProductButton" id="addProduct">
         Dodaj środek
       </button>
-      <label>Ilość wody: <input type="number" step="10" value="1000" /> </label>
+      <label>Ilość wody: <input type="number" name="water" step="10" value="1000" /> </label>
       l
       <button type="submit" class="submit">Dodaj zabieg</button>
       <script>
