c454dca5b06b5888fe688d924a1adfcf646463e8
Mariusz Dąbrowski <mariuszd49@gmail.com>
2021-11-26 18:39:11 +0100

    Add datetime to practise blade

--- FarmAssistant/database/migrations/2021_11_26_140015_add_water_to_agricultural_practices.php
+++ FarmAssistant/database/migrations/2021_11_26_140015_add_water_to_agricultural_practices.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAgriculturalPracticesTable extends Migration
+class AddWaterToAgriculturalPractices extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateAgriculturalPracticesTable extends Migration
      */
     public function up()
     {
-        Schema::create('agricultural_practices', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->date('start');
-            $table->date('end');
-            $table->timestamps();
+        Schema::table('agricultural_practices', function (Blueprint $table) {
+            $table->integer('water');
         });
     }
 
@@ -29,6 +25,8 @@ class CreateAgriculturalPracticesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('agricultural_practices');
+        Schema::table('agricultural_practices', function (Blueprint $table) {
+            $table->dropColumn('water');
+        });
     }
 }
