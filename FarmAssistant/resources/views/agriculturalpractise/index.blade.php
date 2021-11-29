de4a939e6e641fec1dffeb0eebafef0d48563988
Maksymilian Dendura <maksd0410@gmail.com>
2021-11-29 20:38:28 +0100

    Wstępny front do zabiegów

--- FarmAssistant/resources/views/agriculturalpractise/index.blade.php
+++ FarmAssistant/resources/views/agriculturalpractise/index.blade.php
@@ -1,28 +1,36 @@
 @extends('layouts.app', ['activeFarm' => $activeFarm, 'farms' => $farms])
-
 @section('content')
-<table >
-    <thead>
-        <th>Lp.</th>
-        <th>Nazwa zabiegu</th>
-        <th>Pola</th>
-        <th>Data wykonania</th>
-    </thead>
-    <tbody>
-        @foreach ($practises as $practise)
-        <tr>
-            <td data-label="Lp.">{{ $loop->index+1 }}</td>
-            <td data-label="Nazwa zabiegu">{{ $practise->name }}</td>
-            <td data-label="Pola">
-                @foreach ($practise->fields as $field)
-                
-                    <li><a href="{{ route('field.show', [$field->farm_id, $field->id])}}">{{ $field->field_name }}</a></li>
-                @endforeach
-            </td>
-            <td data-label="Data wykonania">{{ $practise->updated_at }}</td>
-        
-        </tr>
-           @endforeach
-    </tbody>
-</table>
+<div class="wrapper">
+  <div class="content">
+    <h1>Zabiegi</h1>
+    <ol>
+      @foreach ($practises as $practise)
+      <li>
+        <div class="field">
+          <div>{{ $loop->index+1 }}. {{ $practise->name }}</div>
+          <div>
+            Uprawa: @foreach ($field->crops as $crop)
+            {{ $crop->name }}
+            @endforeach
+          </div>
+          <div>
+            Pola:
+            <ul>
+              @foreach ($practise->fields as $field)
+              <li>
+                <a
+                  href="{{ route('field.show', [$field->farm_id, $field->id])}}"
+                  >{{ $field->field_name }}</a
+                >
+              </li>
+              @endforeach
+            </ul>
+          </div>
+          <div>Data wykonania: {{ $practise->updated_at }}</div>
+        </div>
+      </li>
+      @endforeach
+    </ol>
+  </div>
+</div>
 @endsection
--- FarmAssistant/resources/views/field/index.blade.php
+++ FarmAssistant/resources/views/field/index.blade.php
@@ -32,34 +32,6 @@
           </div>
         </div>
       </li>
-      <li>
-        <div class="field">
-          <div>
-            <a href="{{ route('field.show', [$field->farm_id, $field->id]) }}"
-              >{{ $loop->index+2 }}. {{ $field->field_name }}</a
-            >
-          </div>
-          <div>Powierzchnia: {{ $field->field_area }} ha</div>
-          <div>
-            Uprawa: @foreach ($field->crops as $crop)
-            {{ $crop->name }}
-            @endforeach
-          </div>
-          <div>
-            Lista działek:
-            <ul>
-              @foreach ($field->cadastralParcels as $parcel)
-              <li>
-                <a
-                  href="{{ route('parcel.show', [$field->farm_id, $field->id, $parcel->id]) }}"
-                  >{{ $parcel->parcel_number}}</a
-                >
-              </li>
-              @endforeach
-            </ul>
-          </div>
-        </div>
-      </li>
       @endforeach
     </ol>
   </div>
