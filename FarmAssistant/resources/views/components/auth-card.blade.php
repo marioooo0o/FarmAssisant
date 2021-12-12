ba77633f59bb4673e070eeb4e2b8403b9ba1fd70
Maksymilian Dendura <maksd0410@gmail.com>
2021-12-12 16:22:20 +0100

    Added text under logo

--- FarmAssistant/resources/views/components/auth-card.blade.php
+++ FarmAssistant/resources/views/components/auth-card.blade.php
@@ -1,6 +1,7 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
+    <div class="text-center">
+        <x-application-logo class="block h-40 w-auto mx-auto" />
+        <h1 class="text-3xl font-extrabold">Farm Assistant</h1>
     </div>
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
